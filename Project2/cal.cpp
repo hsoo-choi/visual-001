bed1aac9dea3dcf7403b4324c7b41e633a86f8a1
hsoo.choi <choihszg@gmail.com>
2023-07-05 08:43:12 +0900

    두번쨰 커밋

---
+++ Project2/cal.cpp
@@ -0,0 +1,18 @@
+int getSum(int a, int b)
+{
+}
+
+int getZegop(int a)
+{
+	
+}
+
+int getMinus(int a, int b)
+{
+	
+}
+
+int getSumSum(int a, int b, int c)
+{
+	
+}
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,12 @@
 #include "pch.h"
-#include "../Project2/main.cpp"
+#include "../Project2/Filename.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, TestName2) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
 }
