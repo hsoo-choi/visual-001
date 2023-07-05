81653c0c22139c303a40ca37e12e13de3df1724d
Hakmin Kim <ringo.cozy@gmail.com>
2023-07-05 09:25:44 +0900

    uniittest add

--- Project2/cal.cpp
+++ Project2/cal.cpp
@@ -1,18 +1,20 @@
 int getSum(int a, int b)
 {
+	return 0;
 }
 
 int getZegop(int a)
 {
-	if (!a) return a* a;
+	if (!a) return 0;
+	return a * a;
 }
 
 int getMinus(int a, int b)
 {
-	
+	return 0;
 }
 
 int getSumSum(int a, int b, int c)
 {
-	
+	return 0;
 }
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -10,3 +10,15 @@ TEST(TestCaseName, TestName2) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
 }
+
+TEST(GetZegopTest, ZeroInput)
+{
+    int result = getZegop(0);
+    EXPECT_EQ(result, 0);
+}
+
+TEST(GetZegopTest, NonZeroInput)
+{
+    int result = getZegop(5);
+    EXPECT_EQ(result, 25);
+}
