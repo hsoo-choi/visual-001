baf6d4252b923bec86165f294b2b7d051eaabaab
hsoo.choi <choihszg@gmail.com>
2023-07-05 09:24:12 +0900

    sum test 추가

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,12 +1,6 @@
 #include "pch.h"
 #include "../Project2/cal.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
-
-TEST(TestCaseName, TestName2) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+TEST(TestCaseName, SumTest) {
+  EXPECT_EQ(5, getSum(2, 3));
 }
